153f9be3d572e6d001cbcb8b19b14613d08b769a
ishida_masakazu <u225210b@gmail.com>
2018-10-23 22:28:22 +0900

    環境変数設定追記

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Amazon::Ecs.configure do |options|
+  options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
+  options[:AWS_secret_key] = ENV['AWS_SECRET_ACCESS_KEY']
+  options[:associate_tag] = ENV['ASSOCIATE_TAG']
+end
